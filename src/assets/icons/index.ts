a895fdc5da88b26216131690c3d7ad52e6a29a93
Atishaya Maharjan <atishaya@soanitech.com>
2023-09-03 19:09:17 -0500

    feat: Added the footer

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,23 @@ export {
 	default as IconLinkDefault,
 	ReactComponent as IconLink,
 } from './icon-link.svg';
+
+export {
+	default as IconFacebookDefault,
+	ReactComponent as IconFacebook,
+} from './icon-facebook.svg';
+
+export {
+	default as IconGithubDefault,
+	ReactComponent as IconGithub,
+} from './icon-github.svg';
+
+export {
+	default as IconInstagramDefault,
+	ReactComponent as IconInstagram,
+} from './icon-instagram.svg';
+
+export {
+	default as IconLinkedInDefault,
+	ReactComponent as IconLinkedIn,
+} from './icon-linkedin.svg';
