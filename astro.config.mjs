f835b2beb20edc206a26d3ee49d05597238fa036
Atishaya7777 <atishaya01.maharjan@cnspartner.com>
2024-07-13 20:24:47 -0500

    feat: Initial draft of setting up the deployment to github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// NOTE: Following this link to change the config to deploy to github pages: https://www.cmsoftdev.com/01-github-deploy/
+	integrations: [],
+	site: `https//www.atishaya7777.github.io`,
+	output: 'static',
+	outDir: './docs',
+	build: {
+		assets: 'astro'
+	}
+});
