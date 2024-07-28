2cb7ee8c505a91709fced3ed1cebfab742c3db6d
Atishaya7777 <atishaya01.maharjan@cnspartner.com>
2024-07-27 19:40:26 -0500

    refactor: Commented out server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	// NOTE: Following this link to change the config to deploy to github pages: https://www.cmsoftdev.com/01-github-deploy/
 	integrations: [],
-	site: `https//www.atishaya7777.github.io`,
+	// site: `https//www.atishaya7777.github.io`,
 	output: 'static',
 	outDir: './docs',
 	build: {
