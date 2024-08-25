4fac719218988a6cc6eed7d92bc78f70d3f9fb30
Atishaya7777 <atishaya01.maharjan@cnspartner.com>
2024-08-25 18:07:37 -0500

    feat: Added new config file for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   // NOTE: Following this link to change the config to deploy to github pages: https://www.cmsoftdev.com/01-github-deploy/
   integrations: [tailwind()],
-  // site: `https//www.atishaya7777.github.io`,
+  site: `https//www.atishaya7777.github.io`,
   output: 'static',
   outDir: './docs',
   build: {
