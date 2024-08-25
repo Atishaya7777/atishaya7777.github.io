2702ae5f87ef7ccb8c79bec4773e55ec3fd4f164
Atishaya7777 <atishaya01.maharjan@cnspartner.com>
2024-08-25 18:01:14 -0500

    feat: Added all the other content

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { Button } from "./Button";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Navbar } from "./Navbar";
+import { Section } from "./Section";
+
+export { Button, Footer, Header, Navbar, Section };
