9f1d4006b864e9cd3edfcd3c0f1358d09a5a6ffd
Atishaya Maharjan <71453175+Atishaya7777@users.noreply.github.com>
2025-08-21 15:39:01 -0500

    feat: Added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   site: `https://www.atishaya7777.github.io`,
   output: 'static',
   outDir: './dist',
